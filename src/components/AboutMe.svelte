849e5978bf3de99862e44e454b1cb614ac3246a8
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2023-04-08 23:14:52 -0600

    Logo luis-barrera
    - Adds props to select color to apply.

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -1,13 +1,12 @@
 <script>
-	import Logo from '../components/Logo.svelte';
+	import Logo from '../components/svg/Logo.svelte';
 </script>
 
 <article class="min-h-screen">
 	<div class="relative top-0 right-0 left-0">
 		<div class="grid place-items-center h-screen">
 			<div class="flex flex-col items-center">
-				<!-- <span>luis-barrera</span> -->
-				<Logo />
+				<Logo width={'70%'} color={'black'} />
 				<h1 class="">Luis Antonio Valdovinos Barrera</h1>
 			</div>
 		</div>
