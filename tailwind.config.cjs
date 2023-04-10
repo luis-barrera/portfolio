e97f449d28fa3cc1ea9bbd357a4aaee3dbd26f0e
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2023-04-09 19:46:39 -0600

    Tailwind: change colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brown': '473c33',
+        'green': 'abc269',
+        'yellow': 'fec868',
+        'pink': 'fda769',
+      }
+    },
   },
   plugins: [],
 }
